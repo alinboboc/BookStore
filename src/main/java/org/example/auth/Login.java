ab3758629f2a2862a814f8ea667196cb71d3b527
Alin Boboc <boboc.alin.ro@gmail.com>
2024-08-05 22:10:52 +0300

    * login function added * small updates in other files

--- src/main/java/org/example/auth/Login.java
+++ src/main/java/org/example/auth/Login.java
@@ -1,11 +1,63 @@
 package org.example.auth;
 
 import org.example.database.DatabaseConnection;
+import org.example.pages.StoreMainPage;
+import org.example.pages.WelcomePage;
+import org.example.tools.Tools;
 
-public class Login {
+import java.sql.SQLException;
+
+public class Login extends Tools {
 
     public Login() {
-        System.out.println("LOGIN");
-        new DatabaseConnection();
+        stringSeparator();
+        System.out.println("You chose to login into your account.");
+        System.out.println("Please enter your username:");
+        String inputUser = sc.nextLine();
+        try {
+            new DatabaseConnection();
+            setQuery("SELECT * FROM users");
+            setStmt(conn.prepareStatement(getQuery()));
+            setRs(getStmt().executeQuery());
+            while (getRs().next()) {
+                usernameCheck(inputUser);
+            }
+        } catch (SQLException ex) {
+            System.out.println("-----SQL_ERROR-----");
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        }
+    }
+
+    private void usernameCheck(String inputUser) throws SQLException {
+        String username = getRs().getString("username");
+        if (username.equals(inputUser)) {
+            passwordCheck();
+        } else {
+            System.out.println("The username you provided is not registered, you will be redirected to the welcome page.");
+            new WelcomePage();
+        }
+    }
+
+    int maxPasswordInputs = 3;
+
+    private void passwordCheck() throws SQLException {
+        if (maxPasswordInputs == 0) {
+            System.out.println("The maximum logins have been reached, you will be returned to the main page.");
+            new WelcomePage();
+        }
+        System.out.println("Enter your password:");
+        String inputPassword = sc.nextLine();
+        if (inputPassword.equals(getRs().getString("userPassword"))) {
+            setUserLoggedIn(true);
+            new StoreMainPage();
+        } else {
+            maxPasswordInputs--;
+            if (maxPasswordInputs != 0) {
+                System.out.println("The password is not ok, you have " + maxPasswordInputs + " attempts.");
+            }
+            passwordCheck();
+        }
     }
 }
